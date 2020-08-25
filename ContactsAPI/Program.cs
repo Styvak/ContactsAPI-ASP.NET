5669f5cddc7bac078f854a328794cfa23f8d20e2
Gascon Nicolas <nicolas.gascon@outlook.fr>
2020-08-25 19:53:53 +0200

    [NEW] Docker & Docker Compose
    [EDIT] Readme

--- ContactsAPI/Program.cs
+++ ContactsAPI/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContactsAPI.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +15,37 @@ namespace ContactsAPI
 {
     public class Program
     {
-        public static void Main(string[] args)
+        /*public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+        }*/
+
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+                await dbContext.Database.MigrateAsync();
+
+                /*var roleManager = serviceScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+                if (!await roleManager.RoleExistsAsync("Admin"))
+                {
+                    var adminRole = new IdentityRole("Admin");
+                    await roleManager.CreateAsync(adminRole);
+                }
+
+                if (!await roleManager.RoleExistsAsync("Poster"))
+                {
+                    var posterRole = new IdentityRole("Poster");
+                    await roleManager.CreateAsync(posterRole);
+                }*/
+            }
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# ContactsAPI ASP.NET
- Contacts REST API using ASP .NET
+# Contacts API
+
+Simple REST Contacts API in ASP.NET Core
+
+## Description
+
+Contacts API is a REST API made as a technical test.
+
+It contains:
+* CRUD for contacts
+* CRUD for skills
+* Account creation and log in
+* Pagination to get a list of contacts
+* A generated Swagger API
+* A dockerize environment using docker compose
+* Interaction with a MSSQL database
+
+## Getting Started
+
+### Installation
+
+* Install docker
+* Run the command:
+```
+docker-compose build
+```
+
+### Executing program
+
+* Run the command:
+```
+docker-compose up
+```
+* Server available at http://localhost:5000/api
+
+## Swagger API
+
+Swagger API available at:
+```
+http://localhost:5000/swagger/index.html
+```
+
+## Authors
+
+Contributors names and contact info:
+
+Nicolas Gascon
+
+## Version History
+
+* 0.1
+    * Initial Release
